1bd6fe7d0707339b192a7786457d2272ded94729
Anadi S Yagnik <99112612+Adiversion@users.noreply.github.com>
2026-09-19 10:05:40 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmToolchain(17)
+    kotlin {
+     jvmToolchain(17)
     }
     buildFeatures {
         compose = true
